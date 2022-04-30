e9d5fa867ae50ea4ba5f4bf0727a677814cf9495
tkhan27 <tkhan27@node1.peerml.netml-pg0.utah.cloudlab.us>
2022-04-30 12:35:53 -0600

    first commit :)

---
+++ client.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify
+import requests, json, time
+import urllib.request
+import threading
+import os
+import subprocess
+import shlex
+from math import trunc
+
+def subrun(com, capture_output=False):
+    stdout = None if not capture_output else subprocess.PIPE
+    universal_newlines = None if not capture_output else True
+    return subprocess.run(shlex.split(com), stdout=stdout, universal_newlines=universal_newlines, check=True)
+
+url = 'https://tkhaniitr-u3f2l0793la7pe20.socketxp.com/'
+header = {"Content-type": "application/json"}
+                #  "Accept": "text/plain"} 
+ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
+print(ip)
+payload = {'spec': 'ok', 'minutes': '180', 'IP': ip}
+response = requests.post(url+"register/", data = json.dumps(payload), headers=header)
+user = response.json()
+print(user)
+start_training = False
+
+def train(master_ip, world_size, job_id, rank):
+    print(master_ip)
+    print(world_size)
+    print(job_id)
+    print(rank)
+    #resp = requests.get(url+ '/downloads/', headers=header)
+    #assert(subrun('curl https://tkhaniitr-u3f2l0793la7pe20.socketxp.com/downloads/ -o outFile.zip').returncode == 0)
+    dir = "file_store/jobid"+str(job_id)+"/"
+    assert(subrun("unzip "+dir+"outFile.zip").returncode == 0)
+    run_train = 'python main.py --master-ip '+master_ip + ' --num-nodes '+str(world_size)+' --rank '+str(rank)
+    assert(subrun(run_train).returncode == 0)
+
+while(True):
+    #print(cnt)
+    time.sleep(60)
+    resp = requests.get(url+"up/"+str(user), headers=header)
+    #cnt = cnt + 1
+    print(resp.text)
+    response = resp.json()
+    if  start_training is False and response['status'] == 'RUNNING': #change it to START
+        start_training = True
+        #_thread.start_new_thread(target = train, args = (response['master_ip'], response['world_size'],))
+        thread = threading.Thread(target = train, args = (response['master_ip'], str(response['world_size'])[:-2], str(response['job_id'])[:-2],response['rank']))
+        thread.start()
+        print('ok')
